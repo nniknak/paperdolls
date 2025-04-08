0323314bb5e83735cbd5b99c31585350e667bfe8
Annika Kohrt <kohrt.annika@gmail.com>
2025-04-08 09:51:40 -0500

    new blob save attempt

--- code.js
+++ code.js
@@ -117,6 +117,25 @@ let state = {
     return myAudio.paused ? myAudio.play() : myAudio.pause();
   }
 
+  // Function to download data to a file
+  function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+  }
+
   function saveDoll(){
     let mannequin = new Image(450, 380);
     mannequin.setAttribute('crossorigin', 'anonymous');
@@ -171,6 +190,5 @@ let state = {
     var dressBlob = canvas.convertToBlob();
     console.log(dressBlob);
 
-    var blobURL = URL.createObjectURL(dressBlob);
-    window.open(blobURL);
+    download(dressBlob, "myDoll.png", "png");
   }
